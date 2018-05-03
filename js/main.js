cdd2f6697e3adaa124a9cf487ecdf96e20bb85f4
lucianasm86 <itsuptocode@gmail.com>
2018-05-02 20:37:10 -0700

    second exercise

--- js/main.js
+++ js/main.js
@@ -1,5 +1,71 @@
-var testOne = "What kind of variable am I?";
-// A string
-var testTwo = 256;
-// A number
-var testThree = false;
+// var testOne = "What kind of variable am I?";
+// // A string
+// var testTwo = 256;
+// // A number
+// var testThree = false;
+// // A Boolean
+// var testFour = 'true';
+// // A boolean that is inside a string
+// var testFive = '128';
+// // A number inside a string
+// var testSix = "";
+// // An empty string
+
+// var testOne = "hello" + "there";
+// // String. "hellothere".
+// var testTwo = '100' + 28;
+// // String. "10028".
+// var testThree = true + 3;
+// // Number. 4.
+// var testFour = 'false' + 5;
+// // String. "false5"
+// var testFive = '128' - 3;
+// // Number. 125.
+// var testSix = "" * 9;
+// // Number. 0.
+// var testSeven = 'zero' - 1;
+// // NaN.
+// var testEight = 'five'.lenght;
+// // Undefined.
+// var testNine = 'five'[0] + 'ever';
+// // String. Fever.
+// var testTen = 'a' > 'b';
+// // Boolean. False.
+// var testEleven = 'number'.lenght < 7;
+// // Boolean. False.
+// var testTwelve = '12' == 12;
+// // Boolean. True.
+// var testThirteen = 'twelve' == 12;
+// // Boolean. False.
+// var testFourteen = '14' === 14;
+// // Boolean. False.
+// var testFifteen = 'Hi there'[0] + 'open sesame'[2] + 'alohamora'[1] + 'eulalia'[4] + 'harkonnen'[4] + 'BATMAN!'[6];
+// // String. "Hello!".
+
+var string1 = "Hello, ";
+var string2 = "my name is ";
+var string3 = "Kingsley Pappagiorgio.";
+
+var combinedStrings = string1 + string2 + string3;
+
+console.log(combinedStrings);
+
+// "Hello, my name is Kingsley Pappagiorgio."
+
+var num1 = 8;
+var num2 = 52;
+var num3 = 60;
+
+var combinedNumbers = num1 + num2 + num3;
+
+console.log(combinedNumbers);
+// 120
+
+var mixedNum1 = 10;
+var mixedString1 = "ten";
+var mixedNum2 = "10";
+
+var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1;
+
+console.log(mixedConcatenation);
+// 1010ten
